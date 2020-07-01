6501fade7124e72b43be1930f0cbe2f49fdc27d1
paragpatil1999 <paragptil1999@gmail.com>
2020-07-01 13:29:50 +0530

    Completed a programming assignment

--- SampleDataStructurs.R
+++ SampleDataStructurs.R
@@ -14,14 +14,7 @@ dim(y) <- c(4,5)
 #Reading data in R
 #1.Read.table,Read.csv =>>(returns data file)
 #2.Read.line
-dataR <- read.csv('/home/kaneki99/Downloads/DataSets-master/airports.csv',nrows = 20)
-dim(dataR)
-print(dataR)
-lat <- dataR[5]
-lon <- dataR[6]
-plot(lon)
-a <- matrix(1:100,nrow = 10,ncol = 10)
-colnames(a) <- rep("parag",10)
-write.csv(a,'/home/kaneki99/Desktop/R.xlsx')
-a 
+dataR <- read.csv('/home/kaneki99/Downloads/hw1_data.csv')
 
+good <- dataR["Month"]==5 & !is.na(dataR["Ozone"])
+mean(dataR[good,"Ozone"])
